cab8f0dc96b91c07af15896422fe4926ce69e67b
stefan22 <stefan.emailacct@gmail.com>
2016-11-05 11:14:40 +0000

    newer and improved

---
+++ config.rb
@@ -0,0 +1,6 @@
+require 'susy'
+
+css_dir = '/public/builds/css'
+sass_dir = '/public/sass'
+javascript_dir = '/public/js'
+output_style = :expanded
--- gruntfile.js
+++ gruntfile.js
@@ -1,116 +1,104 @@
 module.exports = function(grunt) {
-
-  // SL Proj Config.
+  //SL Proj Config.
   grunt.initConfig({
-    //for moving all my js files into one single minimized js file.
-    uglify: {
-      //minimizes javascript
-      options: {
-        banner: '-----------------------top of document -----------------\n\n',
-        footer: '\n\n----------------------end of file ------------------------\n'
-      },
-      builds: {
-        //src and dest inside of public folder
-        src: 'public/builds/src/js/*.js',
-        dest: 'public/builds/dest/js/script.min.js'
-      }
-    }, //uglify ends
-    // copying index.html and bower_components to dest folder
-    concat: {
-      options: {
-        //banner: '\n--------------------top of document -----------------\n\n',
-        //separator:'\n\n-----------------end of a file-----------------------\n\n',
-        //footer: '\n\n--------------------end of document ------------------------\n'
-      },
-      dist1: {
-        //sends index.html to dest folder
-        src: ['index.html'],
-        dest: 'public/builds/dest/index.html'
-      }, //concat1
-
-      dist2: {
-        // sends readme.md to dest folder
-        src: ['README.md'],
-        dest: 'public/builds/dest/assets/README.md'
-      } //concat2
-
-     
-
-    }, // concat ends
-    // copies bower_components to dest
-    copy: {
-      main: {
-        files: [{
-        // includes files within path and its sub-directories
-          expand: true, 
-          src: ['bower_components/**'], 
-          dest: 'public/builds/dest/'
-        }]
-      }
-
-    },//copy entire bower_components to dest
-
-    // process sass from style.scss to dest css
-    sass: {
-      dist: {
+      uglify: {
         options: {
-          style: 'expanded'
+          banner: '-----------------------top of document -----------------\n\n',
+          footer: '\n\n--------------------end of file ---------------------\n'
         },
-        files: [{
-          src: 'public/builds/src/sass/styles.scss',
-          dest:'public/builds/dest/css/styles.css'
-        }]
-      }
-
-    }, //sass
-
-     // helps makes it more automatic
-    connect: {
-      //connect
-      server: {
-        options: {
-          hostname: 'localhost',
-          port: 9000,
-          base: '',
-          livereload: true
+        builds: {
+          src:'public/js/*.js',
+          dest:'public/builds/js/scripts.min.js'
         }
-      }
 
-    }, //connect & livereload
+      }, // uglify
 
-    open: {
-      all: {
-        //gets port from connect configuration
-        path: 'http://localhost:9000'
-      }
+      // copying index.html and bower_components to dest folder
+      concat: {
+        options: {
+          //banner: '\n--------------------top of document ------------\n\n',
+          //separator:'\n\n-----------------end of a file---------------\n\n',
+          //footer: '\n\n--------------------end of document ------------\n'
+        },
+        dist1: {
+          //sends index.html to dest folder
+          src: ['index.html'],
+          dest: 'public/builds/index.html'
+        }, //concat1
 
-    },//open browser
+        dist2: {
+          // sends readme.md to dest folder
+          src: ['README.md'],
+          dest: 'public/builds/README.md'
+        } //concat2
 
-    watch: {
-      // watching everywhere really
-      options: {
-        //run faster
-        spawn: false,
-        livereload: true
-      },
-      scripts: {
-        files: [
-          'index.html',
-          'README.md',
-          'public/builds/src/**/*.html',
-          'public/builds/dest/**/*.html',
-          'public/builds/src/js/*.js',
-          'public/builds/src/sass/**/*.scss',
-          'public/builds/dest/css/*.css',
-          'public/builds/src/js/*.js'
+       
+
+      }, // concat ends
+
+      // compass for sass
+      compass: {
+        dev: {
+          options: {
+            config: 'config.rb'
+          }
+        }
+
+      }, //compass
+
+      // helps makes it more automatic
+      connect: {
+        //connect
+        server: {
+          options: {
+            hostname: 'localhost',
+            port: 9001,
+            base: '',
+            livereload: true
+          }
+        }
+
+      }, //connect & livereload
+
+      open: {
+        all: {
+          //gets port from connect configuration
+          path: 'http://localhost:9001'
+        }
+
+      },//open browser
+
+
+      watch: {
+        // watching everywhere really
+        options: {
+          //run faster
+          spawn: false,
+          livereload: true
+        },
+        scripts: {
+          files: [
+            'public/js/**/*.js'
           ],
-        tasks: ['copy', 'concat', 'sass']  
-      }
+          tasks: ['uglify']  
+        
+        },//scripts
 
-    } //watch
+        html: {
+          files: ['*.html', 'README.md'],
+          tasks: ['concat']
+        
+        },//html
 
+        sass: {
+          files: ['public/sass/*.scss'],
+          tasks: ['compass:dev']
 
-  }); //initConfig
+        } //sass
+
+      } //watch
+
+  }) //initConfig
 
   // :grunt uglify
   grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -118,8 +106,8 @@ module.exports = function(grunt) {
   // :grunt concat
   grunt.loadNpmTasks('grunt-contrib-concat');
 
-  // :grunt sass
-  grunt.loadNpmTasks('grunt-sass');
+  // :grunt compass
+  grunt.loadNpmTasks('grunt-contrib-compass');
 
   // :grunt watch
   grunt.loadNpmTasks('grunt-contrib-watch');
@@ -130,20 +118,13 @@ module.exports = function(grunt) {
   // :grunt open
   grunt.loadNpmTasks('grunt-open');
 
-  // :grunt copy
-  grunt.loadNpmTasks('grunt-contrib-copy');
-
-  //register concat dist1 & dist2 
+//register concat dist1 & dist2 
   grunt.registerTask('dist', ['concat:dist1', 'concat:dist2']);
 
-  // :grunt
-  // default tasks
-  // faster this way - no need to run all of the tasks
-  grunt.registerTask('default', ['open','sass', 'dist', 'uglify', 'connect', 'watch']);
+// :grunt
+// default tasks
+grunt.registerTask('default', ['open', 'compass:dev',  'dist', 'uglify', 'connect', 'watch']);
 
-   // :grunt
-  // prodTasks
-  grunt.registerTask('prodTasks', ['open', 'copy', 'uglify','dist', 'sass', 'connect', 'watch']);
 
 
 }; //wrapper function
